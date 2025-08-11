410f88a9a0ee2d7d8b7caf3e4605278bc76c656e
Vova Babushkin <babushkinvova05@gmail.com>
2025-08-11 23:04:24 +0700

    L1.9

--- L1/L1.9/main.go
+++ L1/L1.9/main.go
@@ -14,11 +14,9 @@ func main() {
 			select {
 			case a, ok := <-ch1:
 				if !ok {
-					println("conveer 1 is closed")
 					close(ch2)
 					return
 				}
-				println("put", a, "from ch1 to ch2")
 				ch2 <- a * 2
 			}
 		}
@@ -31,19 +29,16 @@ func main() {
 			select {
 			case a, ok := <-ch2:
 				if !ok {
-					println("conveer 2 is closed")
 					return
 				}
-				println("get", a, "from ch2")
+				println(a)
 			}
 		}
 	}()
 
 	for i := 0; i < 10; i++ {
-		println("put", i, "to ch1")
 		ch1 <- i
 	}
 	close(ch1)
 	wg.Wait()
-	println("fabric is stopped")
 }
